001d07049453d5cd41448a5acc953aeaec5946ba
Mustafa Terzi <mustafaterzi444@gmail.com>
2025-03-02 23:52:13 +0300

    İlk commit: Çift sayıların toplamını hesaplayan kod

---
+++ script.js
@@ -0,0 +1,12 @@
+let toplam =0;
+let i = 1;
+
+if (i <= 50){
+    for(; i <=50; i++) {
+        if(i % 2 === 0) {
+            toplam +=i
+        }
+    }
+}
+console.log("1 ile 50 arasındaki çift sayıların toplamı:",toplam);
+
